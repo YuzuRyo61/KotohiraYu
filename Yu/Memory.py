8dbff3ea0ff8c68cde9fc90cb5a693bde2fd6e41
YuzuRyo61 <yuzuryo61@yuzulia.com>
2019-04-13 12:38:55 +0900

    データベースを使うための準備中っ！

---
+++ Yu/Memory.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+class KotohiraMemory:
+    def __init__(self):
+        pass
--- Yu/__init__.py
+++ Yu/__init__.py
@@ -5,7 +5,8 @@ License: MIT License (See LICENSE)
 """
 
 from Yu.Yu import Yu
+from Yu.Memory import KotohiraMemory
 from Yu.Util import KotohiraUtil
 from Yu.Timelines import local, home
 
-__all__ = ['Yu', 'KotohiraUtil', 'local', 'home']
+__all__ = ['Yu', 'KotohiraUtil', 'KotohiraMemory', 'local', 'home']
