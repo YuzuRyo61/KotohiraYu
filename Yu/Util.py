2e8ea69aa1daa975cca85145be144bb4ccf95edc
YuzuRyo61 <yuzuryo61@yuzulia.com>
2019-05-09 18:53:04 +0900

    多分必要なかったやつですっ

--- Yu/Util.py
+++ Yu/Util.py
@@ -61,7 +61,3 @@ class KotohiraUtil:
             print('五秒待って読み込みし直しますねっ！')
             sleep(5)
             KotohiraUtil.schedule(func, doTimeList)
-    
-    @staticmethod
-    def parse_userEmoji(acct):
-        return f":@{acct}:"
