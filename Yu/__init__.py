f688c3182f19dcfa35f6b8863b88f4e970aa1a55
YuzuRyo61 <yuzuryo61@yuzulia.com>
2019-07-06 11:00:52 +0900

    タイマークラス、需要ないので削除っ！

--- Yu/__init__.py
+++ Yu/__init__.py
@@ -8,7 +8,6 @@ from Yu.YuChan import YuChan
 from Yu.Memory import KotohiraMemory
 from Yu.Util import KotohiraUtil
 from Yu.Timelines import local, home
-from Yu.Timer import Timer
 from Yu.Web import WEBRUN
 
-__all__ = ['YuChan', 'KotohiraUtil', 'KotohiraMemory', 'local', 'home', 'Timer', 'WEBRUN']
+__all__ = ['YuChan', 'KotohiraUtil', 'KotohiraMemory', 'local', 'home', 'WEBRUN']
