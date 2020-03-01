65acf929fbcb6ee74facfc4dda98763f2fe8d26b
YuzuRyo61 <yuzuryo61@yuzulia.com>
2020-03-01 12:14:07 +0900

    configファイルがない場合はエラーを出すようにっ！

--- Yu/config.py
+++ Yu/config.py
@@ -4,3 +4,5 @@ import toml
 if os.path.isfile("config/config.toml"):
     with open("config/config.toml", mode="r", encoding="utf-8") as cfs:
         config = toml.load(cfs)
+else:
+    raise Exception("Configuration file was not found")
