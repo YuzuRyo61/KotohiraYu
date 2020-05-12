33c23da989aa4879c0e80c9f79c916ac0265bc8c
YuzuRyo61 <yuzuryo61@yuzulia.com>
2020-05-12 13:41:46 +0900

    開発用サーバーを起動できるスクリプトを作成っ！

---
+++ patchScripts/runWebAPIdev.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from Yu.webapi import app
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8787)
