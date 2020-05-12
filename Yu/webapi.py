5813345368ed3fa6f2e4a6628117c2c58ae781bb
YuzuRyo61 <yuzuryo61@yuzulia.com>
2020-05-12 13:42:41 +0900

    ウェブAPIを色々と変更っ！ #31

--- Yu/webapi.py
+++ Yu/webapi.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 from flask_jwt import JWT, jwt_required
 
+from Yu.mastodon import mastodon
 from Yu.config import config
 from Yu.database import DATABASE
-from Yu.models import user_memos
+from Yu.models import user_memos, known_users, fav_rate
 from Yu.webapi_lib import authenticate, identity, model_list, model_get
 
 app = Flask(__name__)
@@ -16,26 +17,38 @@ jwt = JWT(app, authenticate, identity)
 @app.errorhandler(404)
 def NotFound(error):
     return jsonify({
-        "error": {
-            "code": "NOT_FOUND"
-        }
+        "description": "Request API is not found",
+        "error": "Not Found",
+        "status_code": 404
     }), 404
 
 @app.errorhandler(405)
 def MethodNotAllowed(error):
     return jsonify({
-        "error": {
-            "code": "METHOD_NOT_ALLOWED"
-        }
+        "description": "Requested method is not allowed",
+        "error": "Method Not Allowed",
+        "status_code": 405
     }), 405
 
 @app.errorhandler(500)
 def InternalError(error):
     return jsonify({
-        "error": {
-            "code": "INTERNAL_SERVER_ERROR"
-        }
+        "description": "It error occured in server",
+        "error": "Internal Server Error",
+        "status_code": 500
     }), 500
+
+@app.route("/", methods=["GET"])
+def api_index():
+    return jsonify({
+        "working_server": config["instance"]["address"],
+        "features": {
+            "ngword": config["features"]["ngword"],
+            "voteOptout": config["features"]["voteOptout"],
+            "voteOptoutTag": config["features"]["voteOptoutTag"],
+            "newComerGreeting": config["features"]["newComerGreeting"]
+        }
+    })
 
 @app.route("/user_memo", methods=["GET"])
 def list_userMemo():
@@ -45,5 +58,22 @@ def list_userMemo():
 def get_userMemo(memo_time=None):
     return model_get(user_memos, user_memos.memo_time, memo_time)
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/private/known_user", methods=["GET"])
+@jwt_required()
+def list_knownUser():
+    return model_list(known_users)
+
+@app.route("/private/known_user/<ID>", methods=["GET"])
+@jwt_required()
+def get_knownUser(ID=None):
+    return model_get(known_users, known_users.ID, ID)
+
+@app.route("/private/fav_rate", methods=["GET"])
+@jwt_required()
+def list_favRate():
+    return model_list(fav_rate)
+
+@app.route("/private/fav_rate/<ID>", methods=["GET"])
+@jwt_required()
+def get_favRate(ID=None):
+    return model_get(fav_rate, fav_rate.ID, ID)
